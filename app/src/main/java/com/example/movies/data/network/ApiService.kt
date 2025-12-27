92a41c685916a793c695fa6a2331fd94912a92a8
meeten <153901120+meeten@users.noreply.github.com>
2025-12-27 22:59:54 +0200

    feat: implement paginated movie loading with minimum threshold

--- app/src/main/java/com/example/movies/data/network/ApiService.kt
+++ app/src/main/java/com/example/movies/data/network/ApiService.kt
@@ -3,11 +3,13 @@ package com.example.movies.data.network
 import com.example.movies.data.model.MoviesResponseDto
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET("v1.4/movie?page=1&limit=10")
+    @GET("v1.4/movie?limit=10")
     suspend fun loadMovies(
+        @Query("page") page: String,
         @Header("X-API-KEY") apiKey: String
     ): MoviesResponseDto
 }
--- app/src/main/java/com/example/movies/data/repository/MoviesRepository.kt
+++ app/src/main/java/com/example/movies/data/repository/MoviesRepository.kt
@@ -1,5 +1,7 @@
 package com.example.movies.data.repository
 
+import android.util.Log
+import com.example.movies.BuildConfig
 import com.example.movies.data.mapper.MoviesMapper
 import com.example.movies.data.network.ApiFactory
 import com.example.movies.domain.model.Movie
@@ -13,8 +15,15 @@ object MoviesRepository {
     val movies get() = _movies.toList()
 
     suspend fun loadMovies(): List<Movie> {
-        val moviesResponse = apiService.loadMovies()
-        val data = mapper.mapResponseToMovies(moviesResponse)
+        val data = mutableListOf<Movie>()
+        var page = 1
+        while (data.size < 10) {
+            val moviesResponse = apiService.loadMovies("$page", apiKey = BuildConfig.API_KEY)
+            data.addAll(mapper.mapResponseToMovies(moviesResponse))
+            page++
+        }
+
+        Log.d("MOVIES", data.toString())
 
         _movies.addAll(data)
 
