10e9bedcf22b4db8732f0b7699ea7bc99a136e23
meeten <153901120+meeten@users.noreply.github.com>
2025-12-29 21:17:27 +0200

    feat: implement final UI design for MoviesScreen

--- app/src/main/java/com/example/movies/presentation/movie/MoviesScreen.kt
+++ app/src/main/java/com/example/movies/presentation/movie/MoviesScreen.kt
@@ -1,28 +1,38 @@
 package com.example.movies.presentation.movie
 
-
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import com.example.movies.domain.model.Movie
 import com.example.movies.domain.state.MoviesState
+import com.example.movies.ui.theme.blue
 
 @Composable
 fun MoviesScreen() {
@@ -38,7 +48,9 @@ fun MoviesScreen() {
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                CircularProgressIndicator()
+                CircularProgressIndicator(
+                    color = blue
+                )
             }
         }
 
@@ -53,7 +65,7 @@ fun MoviesContent(movies: List<Movie>) {
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
         contentPadding = PaddingValues(4.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
         horizontalArrangement = Arrangement.spacedBy(16.dp),
     ) {
         items(items = movies, key = { it.id }) { movie ->
@@ -64,15 +76,34 @@ fun MoviesContent(movies: List<Movie>) {
 
 @Composable
 fun MovieContent(movie: Movie) {
-    Column() {
-        AsyncImage(
-            model = movie.poster.previewUrl,
-            contentDescription = null,
-            modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(2f / 3f)
-                .clip(RoundedCornerShape(12.dp)),
-            contentScale = ContentScale.Crop
-        )
+    Card(
+        modifier = Modifier
+            .clip(RoundedCornerShape(12.dp)),
+        border = BorderStroke(width = 1.dp, color = Color.Black)
+    ) {
+        Box {
+            AsyncImage(
+                model = movie.poster.previewUrl,
+                contentDescription = null,
+                modifier = Modifier.aspectRatio(2f / 3f),
+                contentScale = ContentScale.FillBounds
+            )
+
+            Box(
+                modifier = Modifier
+                    .padding(15.dp)
+                    .clip(CircleShape)
+                    .background(color = Color.Green.copy(alpha = 0.9f, green = 0.7f))
+                    .align(Alignment.TopEnd)
+            ) {
+                Text(
+                    text = movie.rating,
+                    modifier = Modifier.padding(8.dp),
+                    fontSize = 16.sp,
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
     }
 }
