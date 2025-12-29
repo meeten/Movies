daee586116a8b91365b7afcb9ebe462d5fa286a8
meeten <153901120+meeten@users.noreply.github.com>
2025-12-29 21:13:52 +0200

    feat: add enableEdgeToEdge() for auto setting system bar style

--- app/src/main/java/com/example/movies/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/movies/presentation/main/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.movies.presentation.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import com.example.movies.ui.theme.MoviesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        enableEdgeToEdge()
         setContent {
             MoviesTheme {
                 MainScreen()
