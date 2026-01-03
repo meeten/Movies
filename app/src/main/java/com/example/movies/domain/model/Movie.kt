d5f7663c856866e5b4986ae3cfd4326cd1fb19d8
meeten <153901120+meeten@users.noreply.github.com>
2026-01-03 22:03:10 +0200

    refactor: remove default value for id field

--- app/src/main/java/com/example/movies/domain/model/Movie.kt
+++ app/src/main/java/com/example/movies/domain/model/Movie.kt
@@ -1,10 +1,9 @@
 package com.example.movies.domain.model
 
 data class Movie(
-    val id: Int = 0,
+    val id: Int,
     val name: String,
     val rating: String,
     val year: String,
-    val description: String,
     val poster: Poster
 )
