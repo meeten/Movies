77b7a4910a49a63b52c0df56eece2fcc8a2f2667
meeten <153901120+meeten@users.noreply.github.com>
2026-01-05 22:35:01 +0200

    feat: add method loadMovie for viewModel

--- app/src/main/java/com/example/movies/data/repository/MoviesRepository.kt
+++ app/src/main/java/com/example/movies/data/repository/MoviesRepository.kt
@@ -1,39 +1,52 @@
 package com.example.movies.data.repository
 
 import com.example.movies.BuildConfig
-import com.example.movies.data.mapper.MoviesMapper
+import com.example.movies.data.mapper.MovieMapper
+import com.example.movies.data.mapper.MoviesPreviewMapper
 import com.example.movies.data.network.ApiFactory
 import com.example.movies.domain.model.Movie
+import com.example.movies.domain.model.MoviePreview
 
 object MoviesRepository {
 
     private val apiService = ApiFactory.apiService
-    private val mapper = MoviesMapper()
+    private val moviesPreviewMapper = MoviesPreviewMapper()
+    private val movieMapper = MovieMapper()
 
-    private val _movies = mutableListOf<Movie>()
+    private val _movies = mutableListOf<MoviePreview>()
     val movies get() = _movies.toList()
 
     private var nextFrom: String? = null
-    suspend fun loadMovies(): List<Movie> {
+    suspend fun loadMovies(): List<MoviePreview> {
         val startFrom = nextFrom
 
         if (startFrom == null && movies.isNotEmpty()) {
             return movies
         }
 
-        val responseDto = if (startFrom == null) {
+        val moviesResponse = if (startFrom == null) {
             apiService.loadMovies(getApiKey())
         } else {
             apiService.loadNextMovies(startFrom, getApiKey())
         }
 
-        nextFrom = responseDto.moviesDto.next.takeIf {
-            responseDto.moviesDto.hasNext
+        nextFrom = moviesResponse.moviesDto.next.takeIf {
+            moviesResponse.moviesDto.hasNext
         }
 
-        _movies.addAll(mapper.mapResponseToMovies(responseDto))
+        _movies.addAll(moviesPreviewMapper.mapResponseToMovies(moviesResponse))
 
         return movies
+    }
+
+    suspend fun loadMovie(id: Int): Movie {
+        val movieResponse = apiService.loadMovie(
+            id,
+            getApiKey()
+        )
+
+        val movie = movieMapper.mapperResponseToMovie(movieResponse)
+        return movie
     }
 
     private fun getApiKey(): String {
