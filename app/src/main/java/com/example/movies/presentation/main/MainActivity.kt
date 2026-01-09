0d521c1d1ace01b7ba4d8a51f681c8a3a1b73471
meeten <153901120+meeten@users.noreply.github.com>
2026-01-09 21:48:38 +0200

    feat: pass Application context to for dependency access

--- app/src/main/java/com/example/movies/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/movies/presentation/main/MainActivity.kt
@@ -10,9 +10,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             MoviesTheme(isEnableEdgeToEdge = true) {
-                MainScreen()
+                MainScreen(application)
             }
         }
     }
