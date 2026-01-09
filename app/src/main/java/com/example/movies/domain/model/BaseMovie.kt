1d0499a86d49b8eefb9b3c4d6e9d2065f208fe81
meeten <153901120+meeten@users.noreply.github.com>
2026-01-09 20:56:57 +0200

    refactor: move common properties of Movie models to sealed interface

---
+++ app/src/main/java/com/example/movies/domain/model/BaseMovie.kt
@@ -0,0 +1,24 @@
+package com.example.movies.domain.model
+
+sealed interface BaseMovie {
+    val id: Int
+    val posterUrl: String
+    val rating: String
+}
+
+data class MoviePreview(
+    override val id: Int,
+    override val posterUrl: String,
+    override val rating: String,
+) : BaseMovie
+
+data class MovieDetail(
+    override val id: Int,
+    override val rating: String,
+    override val posterUrl: String,
+    val name: String,
+    val year: Int,
+    val description: String,
+    val trailers: List<Trailer>,
+    val isFavorite: Boolean = false
+) : BaseMovie
