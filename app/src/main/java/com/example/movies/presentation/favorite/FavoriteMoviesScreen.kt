24a0902621ca33b68806630ecc20d5c207cc2624
meeten <153901120+meeten@users.noreply.github.com>
2026-01-09 21:46:22 +0200

    feat: implement FavoriteMoviesScreen

--- app/src/main/java/com/example/movies/presentation/favorite/FavoriteMoviesScreen.kt
+++ app/src/main/java/com/example/movies/presentation/favorite/FavoriteMoviesScreen.kt
@@ -1,8 +1,42 @@
 package com.example.movies.presentation.favorite
 
+import android.app.Application
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.movies.domain.state.FavoriteMoviesState
+import com.example.movies.presentation.home.MoviesContent
+import com.example.movies.ui.theme.blue
 
 @Composable
-fun FavoriteMoviesScreen() {
+fun FavoriteMoviesScreen(
+    application: Application,
+    onMovieClick: (Int) -> Unit,
+) {
+    val viewModel: FavoriteMoviesViewModel =
+        viewModel(factory = FavoriteMoviesViewModelFactory(application))
+    val favoriteMoviesState =
+        viewModel.uiState.observeAsState(FavoriteMoviesState.Initial).value
 
+
+    when (favoriteMoviesState) {
+        FavoriteMoviesState.Initial -> {}
+
+        FavoriteMoviesState.Loading -> {
+            Box(modifier = Modifier.fillMaxSize()) {
+                CircularProgressIndicator(color = blue)
+            }
+        }
+
+        is FavoriteMoviesState.FavoriteMovies -> {
+            MoviesContent(
+                movies = favoriteMoviesState.movies,
+                onMovieClick = onMovieClick,
+            )
+        }
+    }
 }
